8350a565d80b113556deae77d5e4697fe2ed441a
salim <salimtison@gmail.Com>
2021-01-07 10:08:34 +0100

    about.js
    31:33

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,46 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 
-export default function Layout({children, page}){
+export default function Layout({children, page}) {
     return (
         <div className="bg-blue pt-5 text-center min-h-screen">
             <Head>
                 <title>{page}</title>
             </Head>
-            {children}
             <header className="container-lg">
                 <h1 className="text-5xl mb-2"> Crypto Watch</h1>
-                    <div className="inline-grid grid-cols-2 gap-x-10 p-4">
-<Link href="/">
-    <button className="bg-blue-200 p-3 m-2 rounded-3xl hover:shadow-md border-2 border-blue-300">
-        Accueil
-    </button>
-</Link>
-                    </div>
+                <div className="inline-grid grid-cols-2 gap-x-10 p-4">
+                    <Link href="/">
+                        <button className="bg-blue-200 p-3 m-2 rounded-3xl hover:shadow-md border-2 border-blue-300">
+                            Accueil
+                        </button>
+                    </Link>
+                    <Link href="/about">
+                        <button className="bg-blue-200 p-3 m-2 rounded-3xl hover:shadow-md border-2 border-blue-300">
+                            A propos
+                        </button>
+                    </Link>
+                </div>
+                <div>
+                    <Image src="/crypto.jpg" alt="footer-pic" width="400" height="25"
+                           className="rounded-3xl object-cover"/>
+                </div>
             </header>
+            <main className="pt-4 mx-20">{children}</main>
+            <footer className="p-10">
+                <Image src="/crypto.jpg" alt="footer-pic" width="1000" height="30"
+                       className="rounded-3xl object-cover"/>
+                <ul className="pt-10 pb-4 flex justify-around">
+                    <li>A propos</li>
+                    <li>Qui sommes-nous</li>
+                    <li>Suits Conception - 2020</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem iste laboriosam quaerat ratione!
+                    Consequuntur cum doloribus ducimus error esse et facilis fugiat hic illo impedit ipsam laborum
+                    laudantium, modi, nisi nulla optio porro quae quasi repudiandae sint suscipit velit voluptas.</p>
+            </footer>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Layout from "../components/Layout";
 
 export default function Home() {
     return (
-      <Layout page={'Crypto Watch - Accueil'}>
-          <div> Accueil ! </div>
-      </Layout>
+        <Layout page={'Crypto Watch - Accueil'}>
+            <h1>Accueil</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet animi assumenda aut commodi corporis
+                culpa cum dignissimos, dolore eaque esse est eum, excepturi facere fugiat in iste iure minima modi
+                molestiae nesciunt nisi numquam obcaecati placeat provident quae quia quidem, quis quisquam recusandae
+                reiciendis rerum sint totam ut velit vero voluptate. Accusantium aperiam, asperiores atque aut
+                doloremque eaque et ex labore magnam magni minima molestiae nam nemo, neque obcaecati quam, quidem
+                quisquam repellat sequi sit. Eligendi nihil quidem temporibus.</p>
+        </Layout>
     )
 }
